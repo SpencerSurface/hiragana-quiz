d9b3c34d7c40a51ebc736fb8ce545e6abae175dd
Spencer Surface <54449845+SpencerSurface@users.noreply.github.com>
2024-01-09 16:49:11 -0500

    add code to display a question

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,9 +1,14 @@
+let currentQuestionNum = 1;
+const quizLength = 20;
+const mcOptions = 4;
+let quizScore = 0;
+
+let questionKana = [];
+
 const startButton = document.querySelector("#start-button");
 const startDiv = document.querySelector("#start-div");
 const mainEl = document.querySelector("main");
 const quizDiv = createQuizDiv();
-
-let quizRunning = true;
 
 startButton.addEventListener("click", startQuiz);
 
@@ -12,7 +17,15 @@ function startQuiz() {
 
     mainEl.append(quizDiv);
 
-    // document.querySelector("main").append(startDiv);
+    currentQuestionNum = 1;
+    quizScore = 0;
+    questionKana = [];
+
+    for (let i = 0; i < quizLength; i++) {
+        questionKana.push(randomKana());
+    }
+
+    displayNewQuestion();
 }
 
 function createQuizDiv() {
@@ -22,16 +35,47 @@ function createQuizDiv() {
 
     let kanaSpan = document.createElement("span");
     kanaSpan.id = "kana-span";
-    kanaSpan.textContent = "ぁ";
     kanaDiv.append(kanaSpan);
     quizDiv.append(kanaDiv);
 
-    for (let i = 0; i < 4; i++) {
+    for (let i = 0; i < mcOptions; i++) {
         let mcButton = document.createElement("button");
         mcButton.classList.add("mc-button");
-        mcButton.textContent = i + 1;
+        mcButton.textContent = (i + 1) + ". ";
         quizDiv.append(mcButton);
     }
 
+    let nextButton = document.createElement("button");
+    nextButton.textContent = "Next Question";
+    nextButton.setAttribute("disabled", true);
+    quizDiv.append(nextButton);
+
     return quizDiv;
+}
+
+function displayNewQuestion() {
+    console.log(questionKana);
+    let currentKana = questionKana[currentQuestionNum - 1].kana;
+    let currentRomaji = questionKana[currentQuestionNum - 1].romaji;
+    document.querySelector("#kana-span").textContent = currentKana;
+
+    let romajiList = [currentRomaji];
+    let tempRomaji = "";
+    while (romajiList.length < mcOptions) {
+        tempRomaji = randomKana().romaji;
+        if (!romajiList.includes(tempRomaji)) {
+            romajiList.push(tempRomaji);
+        }
+    }
+
+    let mcButtons = document.querySelectorAll(".mc-button");
+    console.log(mcButtons);
+    for (let i = 0; i < mcButtons.length; i++) {
+        console.log(mcButtons[i]);
+        mcButtons[i].textContent = mcButtons[i].textContent + romajiList.splice(Math.floor(romajiList.length * Math.random()), 1)[0];
+    }
+}
+
+function randomKana() {
+    return kanaList[Math.floor((kanaList.length - 1) * Math.random())];
 }
